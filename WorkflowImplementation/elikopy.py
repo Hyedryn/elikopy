5da159ac81349c5cd208998a44a32b667c662294
Mathieu_Simon <mathieu.simon1804@gmail.com>
2020-09-30 19:00:15 +0200

    dti first draft

--- WorkflowImplementation/elikopy.py
+++ WorkflowImplementation/elikopy.py
@@ -1,7 +1,11 @@
 """
  
 """
-
+import numpy as np
+from dipy.io.image import load_nifti, save_nifti
+from dipy.io.gradients import read_bvals_bvecs
+from dipy.core.gradients import gradient_table
+import dipy.reconst.dti as dti
 
 def dicom_to_nifti(folder_path):
     """Convert dicom data into nifti. Converted dicom are then moved to a sub-folder named original_data
@@ -59,6 +63,38 @@ def dti(folder_path):
     ----------
     folder_path: Path to root folder containing all the dicom
     """
+    
+    # load the data======================================
+    data, affine = load_nifti(hardi_fname)
+    bvals, bvecs = read_bvals_bvecs(hardi_bval_fname, hardi_bvec_fname)
+    # create the model===================================
+    gtab = gradient_table(bvals, bvecs)
+    tenmodel = dti.TensorModel(gtab)
+    tenfit = tenmodel.fit(maskdata)
+    # FA ================================================
+    FA = dti.fractional_anisotropy(tenfit.evals)
+    FA[np.isnan(FA)] = 0
+    FA = np.clip(FA, 0, 1)
+    save_nifti('name_fa.nii.gz', FA.astype(np.float32), affine)
+    # colored FA ========================================
+    RGB = dti.color_fa(FA, tenfit.evecs)
+    save_nifti('name_fargb.nii.gz', np.array(255 * RGB, 'uint8'), affine)
+    # Mean diffusivity ==================================
+    MD = dti.mean_diffusivity(tenfit.evals)
+    save_nifti('name_md.nii.gz', MD.astype(np.float32), affine)
+    # Radial diffusivity ==================================
+    RD = dti.radial_diffusivity(tenfit.evals)
+    save_nifti('name_rd.nii.gz', RD.astype(np.float32), affine)
+    # Axial diffusivity ==================================
+    AD = dti.axial_diffusivity(tenfit.evals)
+    save_nifti('name_ad.nii.gz', AD.astype(np.float32), affine)
+    # eigen vectors =====================================
+    save_nifti('name_evecs.nii.gz', tenfit.evecs.astype(np.float32), affine)
+    # eigen values ======================================
+    save_nifti('name_evals.nii.gz', tenfit.evals.astype(np.float32), affine)
+    # diffusion tensor ====================================
+    save_nifti('name_tensor.nii.gz', tenfit.quadratic_form.astype(np.float32), affine)
+
 
 def total_workflow(folder_path, dicomToNifti=False, eddy=False, denoising=False, dti=False):
     """Perform dti and store the data in the out/dti folder.
