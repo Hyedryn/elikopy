dda4d22a1b701838bd9e198b29c68e37027bd40c
Hyedryn <quentin124@hotmail.be>
2020-10-26 08:11:39 +0100

    Converted the library into a python package

---
+++ Tests/test.py
@@ -0,0 +1,7 @@
+import WorkflowImplementation.utils
+import WorkflowImplementation.elikopy
+
+folder_path = 'C:\\Users\\quent\\Desktop\\elikopy_test'
+#WorkflowImplementation.elikopy.patient_list(folder_path)
+
+WorkflowImplementation.elikopy.preproc(folder_path)
