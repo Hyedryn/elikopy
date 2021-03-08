428002975c154a9940c609e8c6cfd184dfef384c
Hyedryn <quentin124@hotmail.be>
2021-03-08 15:47:43 +0100

    Clean inference

--- elikopy/utils.py
+++ elikopy/utils.py
@@ -566,8 +566,27 @@ def synb0DISCO(synb0path):
     Step 3 -  Run inference
     """
     numfold = 5
+    # Get device
+    device = torch.device("cuda")
+
+    T1_input_path = synb0path + "/T1_norm_lin_atlas_2_5.nii.gz"
+    b0_input_path = synb0path + "/b0_d_lin_atlas_2_5.nii.gz"
+
     for i in range(1,numfold):
-        "python3.6 inference.py T1_norm_lin_atlas_2_5.nii.gz b0_d_lin_atlas_2_5.nii.gz b0_u_lin_atlas_2_5_FOLD_"+ i +".nii.gz /dual_channel_unet/num_fold_"+i+"_total_folds_"+numfold+"_seed_1_num_epochs_100_lr_0.0001_betas_\(0.9\,\ 0.999\)_weight_decay_1e-05_num_epoch_*.pth"
+        b0_output_path = synb0path + "/b0_d_lin_atlas_2_5.nii.gz b0_u_lin_atlas_2_5_FOLD_" + i + ".nii.gz"
+        model_path = synb0path + "/dual_channel_unet/num_fold_" + i + "_total_folds_" + numfold + "_seed_1_num_epochs_100_lr_0.0001_betas_\(0.9\,\ 0.999\)_weight_decay_1e-05_num_epoch_*.pth"
+
+        # Get model
+        model = UNet3D(2, 1).to(device)
+        model.load_state_dict(torch.load(model_path))
+
+        # Inference
+        img_model = inference(T1_input_path, b0_input_path, model, device)
+
+        # Save
+        nii_template = nib.load(b0_input_path)
+        nii = nib.Nifti1Image(util.torch2nii(img_model.detach().cpu()), nii_template.affine, nii_template.header)
+        nib.save(nii, b0_output_path)
 
     """
     Step 4 -  Apply
@@ -588,3 +607,70 @@ def synb0DISCO(synb0path):
 
     run_topup = "topup -v --imain=" + synb0path + "/b0_all.nii.gz --datain=" + synb0path + "/acqparams.txt --config=b02b0.cnf --iout=" + synb0path + "/b0_all_topup.nii.gz --out=" + synb0path + "/topup --subsamp=1,1,1,1,1,1,1,1,1 --miter=10,10,10,10,10,20,20,30,30 --lambda=0.00033,0.000067,0.0000067,0.000001,0.00000033,0.000000033,0.0000000033,0.000000000033,0.00000000000067 --scale=0"
 
+
+
+import os
+import sys
+import glob
+import math
+
+from random import shuffle
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+import nibabel as nib
+
+import torch
+import torch.nn as nn
+
+import util
+
+from model import UNet3D
+
+
+def inference(T1_path, b0_d_path, model, device):
+    # Eval mode
+    model.eval()
+
+    # Get image
+    img_T1 = np.expand_dims(util.get_nii_img(T1_path), axis=3)
+    img_b0_d = np.expand_dims(util.get_nii_img(b0_d_path), axis=3)
+
+    # Pad array since I stupidly used template with dimensions not factorable by 8
+    # Assumes input is (77, 91, 77) and pad to (80, 96, 80) with zeros
+    img_T1 = np.pad(img_T1, ((2, 1), (3, 2), (2, 1), (0, 0)), 'constant')
+    img_b0_d = np.pad(img_b0_d, ((2, 1), (3, 2), (2, 1), (0, 0)), 'constant')
+
+    # Convert to torch img format
+    img_T1 = util.nii2torch(img_T1)
+    img_b0_d = util.nii2torch(img_b0_d)
+
+    # Normalize data
+    img_T1 = util.normalize_img(img_T1, 150, 0, 1, -1)
+    max_img_b0_d = np.percentile(img_b0_d, 99)
+    min_img_b0_d = 0
+    img_b0_d = util.normalize_img(img_b0_d, max_img_b0_d, min_img_b0_d, 1, -1)
+
+    # Set "data"
+    img_data = np.concatenate((img_b0_d, img_T1), axis=1)
+
+    # Send data to device
+    img_data = torch.from_numpy(img_data).float().to(device)
+
+    # Pass through model
+    img_model = model(img_data)
+
+    # Unnormalize model
+    img_model = util.unnormalize_img(img_model, max_img_b0_d, min_img_b0_d, 1, -1)
+
+    # Remove padding
+    img_model = img_model[:, :, 2:-1, 2:-1, 3:-2]
+
+    # Return model
+    return img_model
