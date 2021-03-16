f7b8af4a1c7e0ee1fc8934a3bd1849e32797b03c
Quentin Dessain <5383293+Hyedryn@users.noreply.github.com>
2021-03-16 12:07:12 +0100

    Delete tests directory

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-import WorkflowImplementation.utils
-import WorkflowImplementation.elikopy
-
-folder_path = 'C:\\Users\\quent\\Desktop\\elikopy_test'
-#WorkflowImplementation.elikopy.patient_list(folder_path)
-
-WorkflowImplementation.elikopy.preproc(folder_path)
