0853b31719cfa4f660f0fcdcd00f88666490d3d9
Mathieu_Simon <mathieu.simon1804@gmail.com>
2021-03-18 13:46:04 +0100

    fix preproc core

--- elikopy/core.py
+++ elikopy/core.py
@@ -232,7 +232,7 @@ class Elikopy:
                     "wrap": "python -c 'from elikopy.individual_subject_processing import preproc_solo; preproc_solo(\"" + folder_path + "/subjects\",\"" + p + "\",eddy=" + str(
                         eddy) + ",denoising=" + str(denoising) + ",reslice=" + str(reslice) + ",gibbs=" + str(
                         gibbs) + ",topup=" + str(topup) + ",starting_state=" + str(starting_state) + ",bet_median_radius=" + str(
-                        bet_median_radius) + ",bet_dilate=" + str(bet_dilate) + ",bet_numpass=" + str(bet_numpass) + ",cuda=" + str(cuda) + ",cuda_name=" + str(cuda_name) + ",s2v=" + str(s2v) + ",olrep=" + str(olrep) + ")'",
+                        bet_median_radius) + ",bet_dilate=" + str(bet_dilate) + ",bet_numpass=" + str(bet_numpass) + ",cuda=" + str(cuda) + ",cuda_name=\"" + str(cuda_name) + "\",s2v=[" + str(s2v[0]) + "," + str(s2v[1]) + "," + str(s2v[2]) + ",\"" + str(s2v[3]) + "\"],olrep=[" + str(olrep[0]) + "," + str(olrep[1]) + "," + str(olrep[2]) + ",\"" + str(olrep[3]) + "\"])'",
                     "job_name": "preproc_" + p,
                     "ntasks": 1,
                     "cpus_per_task": 8,
