06917f7d41511cb69284e4133e195f3bc5f54f4c
Hyedryn <quentin124@hotmail.be>
2021-04-22 20:10:33 +0200

    Fix makedir biasfield

--- elikopy/individual_subject_processing.py
+++ elikopy/individual_subject_processing.py
@@ -419,6 +419,7 @@ def preproc_solo(folder_path, p, reslice=False, denoising=False,gibbs=False, top
     if biasfield and starting_state!="report":
 
         #import SimpleITK as sitk
+        makedir(folder_path + '/' + patient_path + "/dMRI/preproc/biasfield/", folder_path + '/' + patient_path + "/dMRI/preproc/preproc_logs.txt", log_prefix)
 
         if eddy:
             inputImage = folder_path + '/' + patient_path + '/dMRI/preproc/eddy/' + patient_path + '_eddy_corr.nii.gz'
