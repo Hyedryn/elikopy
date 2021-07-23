23dc18980f78566c8ce6f64c288e7c2822ffbe9e
Hyedryn <quentin124@hotmail.be>
2021-07-23 12:48:19 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -21,10 +21,15 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'numpy',
         'dipy',
         'dmipy',
+        'lxml',
+        'PyPDF2',
+        'fpdf',
+        'matplotlib',
+        'cvxpy'
     ],
 )
